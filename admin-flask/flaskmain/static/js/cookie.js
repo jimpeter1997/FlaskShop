32c17fd97762d29c2a0168eb814d69a8ce8936cf
qzef177 <qzef177@163.com>
2021-04-12 02:06:29 +0800

    2021-04-11 活动修改完成

--- admin-flask/flaskmain/static/js/cookie.js
+++ admin-flask/flaskmain/static/js/cookie.js
@@ -9,3 +9,41 @@ function getCookie(name){
     var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
     return r ? r[1] : undefined ;
 }
+
+//  判断是否有特殊字符
+function RegeMatch(){
+    var pattern = new RegExp("[~'!@#$%^&*()-+_=:]");
+    if($("#name").val() != "" && $("#name").val() != null){
+        if(pattern.test($("#name").val())){
+            alert("非法字符！");
+            $("#name").attr("value","");
+            $("#name").focus();
+            return false;
+        }
+    }
+}
+
+// 判断是否符合某个正则表达式
+// testd_string： 测试字符
+// pattern ： 正则表达式  var pattern = new RegExp("[~'!@#$%^&*()-+_=:]");
+// var startTimeForm = /^[0-9]{4}-[0-1]?[0-9]{1}-[0-3]?[0-9]{1} 00:00:00$/;
+// return ： true 为不符合; false 为符合
+function is_correct_re(testd_string, pattern){
+    if(testd_string == "" || testd_string == null){
+        return true;
+    }else if(!pattern.test(testd_string)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function is_correct_string(testd_string, pattern){
+    if(testd_string == "" || testd_string == null){
+        return true;
+    }else if(pattern.test(testd_string)){
+        return true;
+    }else{
+        return false;
+    }
+}
--- admin-flask/flaskmain/views/activities.py
+++ admin-flask/flaskmain/views/activities.py
@@ -39,8 +39,79 @@ def activities():
         return jsonify(resCode=0, msg="删除成功")
     # 修改活动
     if request.method == "PUT":
-        print("PUT")
-        # put_data = 
+        # var data = {
+        #     "id": id,
+        #     "activity_name": activity_name,
+        #     "start_time": start_time,
+        #     "close_time": close_time,
+        #     "activity_desc": activity_desc,
+        #     "off_percent": off_percent,
+        #     "package_time": package_time,
+        #     "is_active": is_active
+        # };
+        print("PUT is asked")
+        put_data = request.get_json()
+        id = put_data.get("id")
+        activity_name = put_data.get("activity_name")
+        activity_desc = put_data.get("activity_desc")
+        start_time = put_data.get("start_time")
+        close_time = put_data.get("close_time")
+        off_percent = put_data.get("off_percent")
+        package_time = put_data.get("package_time")
+        is_active = put_data.get("is_active")
+        print("id = ", id, "type(id) = ", type(id))
+        print("activity_name = ", activity_name, "type(activity_name) = ", type(activity_name))
+        print("activity_desc = ", activity_desc, "type(activity_desc) = ", type(activity_desc))
+        print("start_time = ", start_time, "type(start_time) = ", type(start_time))
+        print("close_time = ", close_time, "type(close_time) = ", type(close_time))
+        print("off_percent = ", off_percent, "type(off_percent) = ", type(off_percent))
+        print("package_time = ", package_time, "type(package_time) = ", type(package_time))
+        print("is_active = ", is_active, "type(is_active) = ", type(is_active))
+        # 这里有一个小bug，不要把bealean类型放到all里面去判断，true的时候没有问题，但是false的时候会出错
+        if not all([id, activity_name, activity_desc, start_time, close_time, off_percent, package_time]):
+            return jsonify(resCode=1, msg="数据不完整！")
+        # ac = Activity()
+        try:
+            ac = Activity.query.filter_by(id=int(id)).first()
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(resCode=1, msg="数据库链接错误，请联系管理员处理")
+        if ac == None:
+            return jsonify(resCode=1, msg="输入id有误，请重新测试")
+
+        if is_string_validate(activity_name) or is_string_validate(activity_desc):
+            return jsonify(resCode=1, msg="活动名称和活动描述不能包含特殊字符")
+        ac.activity_name = activity_name
+        ac.activity_desc = activity_desc
+
+        try:
+            ac.start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+            ac.close_time = datetime.strptime(close_time, "%Y-%m-%d %H:%M:%S")
+            ac.package_time = datetime.strptime(package_time, "%Y-%m-%d %H:%M:%S")
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(resCode=1, msg="时间格式不正确，或者时间错误")
+
+        print("ac.start_time > ac.close_time = ", ac.start_time > ac.close_time)
+        print("ac.close_time >= ac.package_time = ", ac.close_time >= ac.package_time)
+
+        if (ac.start_time > ac.close_time) or (ac.close_time >= ac.package_time):
+            # 判断时间是否正确
+            return jsonify(resCode=1, msg="发货时间要大于活动截至时间，活动截至时间要大于活动开始时间")
+
+        if not isinstance(is_active, bool):
+            return jsonify(resCode=1, msg="输入是否激活数据错误")
+        ac.is_active = is_active
+        if int(off_percent) >= 100 and int(off_percent) > 0:
+            return jsonify(resCode=1, msg="输入的折扣不正确")
+        ac.off_percent = int(off_percent)
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(resCode=1, msg="数据库链接有误，请联系管理员处理")
+
         return jsonify(resCode=0, msg="修改成功")
     # 添加活动
     if request.method == "POST":
@@ -93,7 +164,7 @@ def activity():
     return render_template('/admin/add_activity.html',context=context, form=form)
 
 
-@admin_views.route('/add/new/activity', methods=["GET", "POST"])
+@admin_views.route('/add/new/activity', methods=["GET", "POST", "DELETE", "PUT"])
 @login_required
 def new_activity():
     if request.method == "POST":
@@ -121,7 +192,8 @@ def new_activity():
         # print("is_active = ", type(is_active))
         # print("count = ", type(count))
         # print("activity_desc = ", activity_desc)
-        if not all([activity_name, start_time, close_time, activity_desc, off_percent, package_time, is_active]):
+        # 这里有一个小bug，不要把bealean类型放到all里面去判断，true的时候没有问题，但是false的时候会出错
+        if not all([activity_name, start_time, close_time, activity_desc, off_percent, package_time]):
             # 判断数据是否为空
             return jsonify(resCode=1, msg="数据不完整")
 
@@ -139,7 +211,7 @@ def new_activity():
             current_app.logger.error(e)
             return jsonify(resCode=1, msg="时间格式不正确，或者时间错误")
 
-        if ac.start_time < ac.close_time and ac.close_time <= ac.package_time:
+        if (ac.start_time > ac.close_time) or (ac.close_time >= ac.package_time):
             # 判断时间是否正确
             return jsonify(resCode=1, msg="发货时间要大于活动截至时间，活动截至时间要大于活动开始时间")
 
